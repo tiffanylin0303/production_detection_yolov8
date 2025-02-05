e7b4cd535bb0e91d5ef84d22fc95240ab1ae300e
Tiffany <tiffany.lin.0303@gmail.com>
2025-02-05 13:58:35 +0800

    return data augmentation txt path

--- DetectClass.py
+++ DetectClass.py
@@ -1,6 +1,7 @@
 import os
 
 path = ['dataset_test/train/labels', 'dataset_test/valid/labels', 'dataset_test/test/labels']
+image_path = ['dataset_test/train/images', 'dataset_test/valid/images', 'dataset_test/test/images']
 
 # check whether the sublist contains "10", "2", "0" (frequently appearing categories in dataset)
 def contains_forbidden_strings(s):
@@ -35,6 +36,7 @@ def data_augmentation_path(path):
     
     img_class = [0]*11  # count raw dataset annotation class distribution
     img_increase = []   # do data augmentation image class list
+    img_increase_path = []  # do data augmentation label path
     img_increase_list = [] # do data augmentation image path
     for i in range(len(txt_content_list)):
         for j in range(len(txt_content_list[i])):
@@ -45,8 +47,16 @@ def data_augmentation_path(path):
         if all(not contains_forbidden_strings(x) for x in txt_content_list[i]) and \
             all(contains_need_strings(y) for y in txt_content_list[i]):
             img_increase.append(txt_content_list[i])
-            img_increase_list.append(img_list[i])
-    return txt_content_list, img_class, img_increase, img_increase_list
+            img_increase_path.append(img_list[i])
+            new_path = img_list[i].replace("labels", "images").replace(".txt", ".jpg")
+            if os.path.exists(new_path):
+                img_increase_list.append(new_path)
+            else:
+                print(f"File does not exist: {new_path}")
+            # img_increase_list.append(img_list[i].replace("labels", "images").replace(".txt", ".jpg"))
+    return txt_content_list, img_class, img_increase, img_increase_list, img_increase_path
+    # return img_increase_list
+# print(data_augmentation_path(path))
 
 if __name__ == "__main__":
     data_augmentation_path(['dataset_test/train/labels', 'dataset_test/valid/labels', 'dataset_test/test/labels'])
