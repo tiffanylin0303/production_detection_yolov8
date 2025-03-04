cfb87a76c782e5605740f33016944891427e5fd6
tiffany <tiffany.lin.0303@gmail.com>
2025-03-04 13:09:21 +0800

    gaussian blur

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -7,18 +7,31 @@ class_names = ['CPU_FAN_NO_Screws', 'CPU_FAN_Screw_loose', 'CPU_FAN_Screws', 'CP
 txt_content_list, img_class, img_list, img_increase, img_increase_path = DetectClass.data_augmentation_path(train_path)
 
 # Local Thresholding
-# Read grayscale images
+# for i in range(10):
+#     image_path = img_list[i].replace("labels", "images").replace(".txt", ".jpg")
+#     original_image= cv2.imread(image_path)
+#     # Read grayscale images
+#     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    
+#     # Thresholding
+#     thresh_value = 128
+#     _, binary_image = cv2.threshold(image, thresh_value, 255, cv2.THRESH_BINARY)
+
+#     # show original images and local thresholding images
+#     cv2.imshow('Original Image', original_image)
+#     cv2.imshow('Binary Image', binary_image)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+# GaussianBlur
 for i in range(10):
     image_path = img_list[i].replace("labels", "images").replace(".txt", ".jpg")
+    # Read images
     original_image= cv2.imread(image_path)
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    
-    # Thresholding
-    thresh_value = 128
-    _, binary_image = cv2.threshold(image, thresh_value, 255, cv2.THRESH_BINARY)
+    image_gaussian = cv2.GaussianBlur(original_image, (15, 15), 0) # 指定區域單位為 (15, 15)
 
-    # show original images and local thresholding images
+    # show original images and gaussianblur images
     cv2.imshow('Original Image', original_image)
-    cv2.imshow('Binary Image', binary_image)
+    cv2.imshow('Gaussianblur image Image', image_gaussian)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
